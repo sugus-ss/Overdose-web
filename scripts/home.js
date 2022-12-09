8dde22bb0a909bae6ffec87e7a2f4edd2bb1e703
Miran-Mirantee <killerm11805@hotmail.com>
2022-12-09 20:43:11 +0700

    Put feature showcase into a module

--- scripts/home.js
+++ scripts/home.js
@@ -2,6 +2,7 @@ import footer from "./footer.js";
 import navBar from "./navbar.js";
 import scrollIntoView from "./scrollToView.js";
 import howToUse from "./howToUseSection.js";
+import featureShowcase from "./featureShowcase.js";
 
 let howToUseImg1 = "./img/home_app.jpg";
 let howToUseHeader1 = "Home page";
@@ -16,9 +17,20 @@ let howToUseImg4 = "./img/patient_app.jpg";
 let howToUseHeader4 = "My Patients Page";
 let howToUsePara4 = "If you are a caregiver, you may face a problem where you are unable to remember the details of each patient due to the large number and variety of patients. You may have to carry a lot of documents to look through which wastes time and resources on printing documents. This system has put your patient information into the application, if you want to know the details of any patient, you can enter your patient name in the system. The system displays the patient details you want to know instantly, saving time and resources spent printing paperwork.";
 
+let featureImg1 = './img/Chart.webp';
+let featureTxt1 = 'Chart and Reports';
+let featureImg2 = './img/red-alarm.jpg';
+let featureTxt2 = 'Alarm in Application';
+let featureImg3 = './img/medicine.webp';
+let featureTxt3 = 'Filter Medicine';
+let featureImg4 = './img/health.jpg';
+let featureTxt4 = 'Sensor check health';
 
 const howToUseSection1 = howToUse(1, howToUseImg1, howToUseHeader1, howToUsePara1);
 const howToUseSection2 = howToUse(2, howToUseImg2, howToUseHeader2, howToUsePara2);
 const howToUseSection3 = howToUse(1, howToUseImg3, howToUseHeader3, howToUsePara3);
 const howToUseSection4 = howToUse(2, howToUseImg4, howToUseHeader4, howToUsePara4);
+
+const featureShowcase1 = featureShowcase(featureImg1, featureTxt1, featureImg2, featureTxt2, featureImg3, featureTxt3, featureImg4, featureTxt4);
+
 
